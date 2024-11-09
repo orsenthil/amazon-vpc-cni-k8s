74bcbba71590e7b19af0a0eec0f9c6597e1005c5
Senthil Kumaran <senthilx@amazon.com>
2024-11-09 13:55:02 +0000

    Updated awsutils go.

--- pkg/vpc/vpc.go
+++ pkg/vpc/vpc.go
@@ -14,6 +14,7 @@ package vpc
 
 import (
 	"errors"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 
 	"github.com/aws/amazon-vpc-cni-k8s/pkg/utils/logger"
 )
@@ -119,7 +120,8 @@ func GetInstance(instanceType string) (InstanceTypeLimits, bool) {
 	return instance, ok
 }
 
-func SetInstance(instanceType string, eniLimit int, ipv4Limit int, defaultNetworkCardIndex int, networkCards []NetworkCard, hypervisorType string, isBareMetalInstance bool) {
-	instanceNetworkingLimits[instanceType] = New(eniLimit, ipv4Limit, defaultNetworkCardIndex, networkCards,
-		hypervisorType, isBareMetalInstance)
+// TODO (senthilx) - Type for instanceType and hypervisorType is now changed.
+func SetInstance(instanceType ec2types.InstanceType, eniLimit int, ipv4Limit int, defaultNetworkCardIndex int, networkCards []NetworkCard, hypervisorType ec2types.InstanceTypeHypervisor, isBareMetalInstance bool) {
+	instanceNetworkingLimits[string(instanceType)] = New(eniLimit, ipv4Limit, defaultNetworkCardIndex, networkCards,
+		string(hypervisorType), isBareMetalInstance)
 }
